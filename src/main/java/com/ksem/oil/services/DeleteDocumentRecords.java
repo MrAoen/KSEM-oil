f3393f42823da65e8e9b11d9abdfd4975ba5445a
helloUsername <dmitry.litvyak@gmail.com>
2021-06-18 06:04:09 +0300

    refactornig

--- src/main/java/com/ksem/oil/services/DeleteDocumentRecords.java
+++ src/main/java/com/ksem/oil/services/DeleteDocumentRecords.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ksem.oil.domain.dto.RecordOwner;
 import com.ksem.oil.domain.dto.TransportMessage;
-import com.ksem.oil.domain.repository.GasSalesRepository;
-import com.ksem.oil.domain.repository.MoneyTransactionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class DeleteDocumentRecords implements MessageProcessor<RecordOwner>{
+public class DeleteDocumentRecords implements MessageProcessor<RecordOwner> {
 
     private final GasSalesService gasSalesService;
     private final MoneyTransactionService moneyTransactionService;
@@ -23,10 +21,11 @@ public class DeleteDocumentRecords implements MessageProcessor<RecordOwner>{
     public RecordOwner convertEntityFromMessage(TransportMessage message) {
         RecordOwner dto = null;
         try {
-             dto = objectMapper.readValue(message.getPayload(),RecordOwner.class);
-            if(dto.getExtId() != null){
+            dto = objectMapper.readValue(message.getPayload(), RecordOwner.class);
+            if (dto.getExtId() != null) {
                 var deleted = gasSalesService.delete(dto.getExtId());
                 var deletedM = moneyTransactionService.delete(dto.getExtId());
+                if(deleted-deletedM != 0 ) log.warn("deleted different rows from source: {} from sales and {} from money",deleted,deletedM);
             }
         } catch (JsonProcessingException e) {
             log.error("Can't convert payload to RecordOwner {} from {}", message.getIndex(), message.getSender());
